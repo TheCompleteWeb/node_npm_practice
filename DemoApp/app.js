830fa59c215c2469ee734edd06fef53f2e5e2a73
neogeoss <neogeo-s@hanmail.net>
2018-05-26 07:21:28 +0000

    faker library practice

---
+++ DemoApp/app.js
@@ -0,0 +1,10 @@
+var catme = require('cat-me');
+var joke = require('knock-knock-jokes');
+
+console.log("FROM APP JS");
+
+
+
+console.log(catme());
+
+console.log(joke());
---
+++ DemoApp/listProducts.js
@@ -0,0 +1,14 @@
+var faker = require('faker');
+
+console.log('================================');
+console.log('      WELCOME TO MY SHOP!');
+console.log('================================');
+
+
+
+for(var i = 0; i<10; i++){
+    var productName = faker.commerce.product();
+    var price = faker.commerce.price();
+    console.log(productName + " - $" + price);
+}
+
